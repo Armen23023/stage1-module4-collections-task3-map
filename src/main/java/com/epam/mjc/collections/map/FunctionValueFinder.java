3e9f9a601d6eb5e0d038b7187ba1986ee4dc2fe9
Armen23023 <mirzoyana805@gmail.com>
2023-02-09 22:16:43 +0400

    WIP

--- src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
+++ src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
@@ -1,15 +1,27 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FunctionValueFinder {
     public boolean isFunctionValuePresent(List<Integer> sourceList, int requiredValue) {
         Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
-        return true;
+        for (Map.Entry<Integer, Integer> entry : functionMap.entrySet()) {
+            if (entry.getValue() == requiredValue){
+                return true;
+            }
+        }
+        return false;
     }
 
     private Map<Integer, Integer> calculateFunctionMap(List<Integer> sourceList) {
-        return null;
+        Map<Integer,Integer> map = new HashMap<>();
+        int value = 0;
+        for (int i = 0; i < sourceList.size(); i++) {
+            value = 5 * sourceList.get(i) +2;
+            map.put(sourceList.get(i),value);
+        }
+        return map;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,9 +1,20 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
-        return null;
+        Map<String, Integer> map = new HashMap<>();
+        for (Map.Entry<Integer, String> entry : sourceMap.entrySet()) {
+            if (map.containsKey(entry.getValue())) {
+                if (map.get(entry.getValue()) > entry.getKey()) {
+                    map.put(entry.getValue(), entry.getKey());
+                }
+            } else {
+                map.put(entry.getValue(), entry.getKey());
+            }
+        }
+        return map;
     }
 }
