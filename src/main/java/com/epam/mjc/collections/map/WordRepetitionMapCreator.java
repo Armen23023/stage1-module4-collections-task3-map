f40752b753186ceae968d67f28ff02b763896ea0
Armen23023 <mirzoyana805@gmail.com>
2023-02-09 20:36:20 +0400

    WIP

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,23 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String ,Integer> map = new LinkedHashMap<>();
+        if (sentence.length()==0) return map;
+        String s1 = sentence.toLowerCase(Locale.ROOT);
+        String[] s = s1.split("[ .,]+");
+        for (String value : s) {
+            if (map.containsKey(value)) {
+                map.put(value,map.get(value)+1);
+            }else {
+                map.put(value,1);
+            }
+        }
+        return map;
     }
 }
